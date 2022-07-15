a13157e32747e1a97fc77cb18f5ceb59b3d6e048
Lee Taesung <julyuniverse@naver.com>
2022-07-15 10:34:19 +0900

    2022-07-15 Update

--- src/main/java/com/onepromath/lms/WebConfig.java
+++ src/main/java/com/onepromath/lms/WebConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// 개발 단계에서 front end(react)와 back end(spring boot)를 분리 개발하면서 필요한 cors 설정
+// 운영 단계에 적용할 땐 apache에서 front end 주소와 back end 주소를 통일시키기 때문에 cors error가 발생되지 않는다. 그러므로 아래 설정은 필요 없어지고 되고 무시하여도 좋다.
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
--- src/main/java/com/onepromath/lms/controller/LearningController.java
+++ src/main/java/com/onepromath/lms/controller/LearningController.java
@@ -32,7 +32,7 @@ import java.util.ArrayList;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/lms-backend")
+@RequestMapping("/api/lms-backend") // 운영 단계에서는 apache에서 요청 주소를 www.example.com/api/lms-backend로 설정하기 때문에 해당 @RequestMapping은 주석 처리하거나 삭제하도록 한다.
 public class LearningController { // 학습
     private final LearningService learningService;
 
